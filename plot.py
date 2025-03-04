d4692fa83653ee8b3b677775eee84fbbbee1c15a
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-04 20:30:36 -0300

    add plot example with matplotlib

---
+++ plot.py
@@ -0,0 +1,38 @@
+'''
+Graph with Matplotlib.
+
+In this case, also using tkinter to display the graph in a window.
+'''
+
+# Import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib
+import random
+
+# Set the backend to TkAgg
+matplotlib.use("TkAgg") 
+
+# X and Y values
+x = range(15)
+y = list(map(lambda x: x**2, x))
+
+# Add labels
+plt.xlabel("X")
+plt.ylabel("Y")
+
+# Add a title
+plt.title("Squaring function")
+
+# Add a grid
+plt.grid()
+
+# Plot the graph
+plt.plot(x, y)
+
+# Optional: save the graph as a .png.
+# Seems it needs to go after .plot() but before .show()
+# If not, only saves a white image
+plt.savefig("plot.png")
+
+# Show the graph
+plt.show()
