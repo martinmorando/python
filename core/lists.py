092541fe15a68d8d4fb54c427395e80dfc35bca0
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-14 13:26:49 -0300

    add .pop(), range(), and move output as comments

--- core/lists.py
+++ core/lists.py
@@ -1,25 +1,37 @@
+'''
+    Lists
+'''
+# Create lists
 empty = []
 strings = ["A", "B", "C"]
 numbers = [1, 2, 3]
 mixed = ["A", 1, 0.99, True]
 
-# Access first item
+# Create a list with integers from 0 to 99
+# list() is necessary because range() returns a range object
+usingRange = list(range(100)) # [0, 1, 2, 3,..., 98, 99]
+
+# Access first item of "strings" list
 first_item = strings[0]
 
-# Add item
-strings.append("D")
+# Add item to "strings" list
+strings.append("D")           # ["A", "B", "C", "D"]
 
 # Remove item (first match only)
-strings.remove("D") 
+strings.remove("B")           # ["A", "C", "D"]
 
-# Modify list
-strings[0] = "AA"
-print(strings)
+# Remove last item
+strings.pop()                 # ["A", "C"]
+
+# Modify 1st item from "strings" list
+strings[0] = "AA"             # ["AA", "C"]
 
 # Add lists
-mixed_2 = strings + numbers
-mixed_3 = strings + numbers + [2] # Add list that contains one item
-print(mixed_2)
+mixed_2 = strings + numbers   # ["AA", "C", 1, 2, 3]
+
+# Add a list that contains one item
+mixed_3 = strings + [4]       # ["AA", "C", 4]
+# This is wrong: mixed_4 = strings + 4
 
 # Zip
-print(list(zip(numbers, strings)))
+list(zip(numbers, strings))
