9b5b772f4e79155b724cdadfdb2a71686538db3b
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-14 13:32:52 -0300

    add tuples

---
+++ core/tuples.py
@@ -0,0 +1,16 @@
+'''
+    Tuples
+'''
+t = ("apple", "banana", "orange")
+
+# Access 2nd item
+firstItem = t[1]              # "banana"
+
+# Number of elements
+length = len(t)               # 3
+
+# Find index of value
+index = t.index("orange")     # 2
+
+# Count number of occurences
+count = t.count("banana")     # 1
