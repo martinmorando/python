43dc6e35ec72f47da3003694fda08d1e67a093aa
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-21 13:09:40 -0300

    add 002-numbers.py and refactor file names to reorder

---
+++ core/002-numbers.py
@@ -0,0 +1,14 @@
+'''
+    Numbers
+'''
+
+# Declare and assign numeric value
+x = 3.14
+
+# Basic operations
+print(2 + 3)  # 5
+print(2 - 3)  # -1
+print(2 * 3)  # 6
+print(2 / 3)  # 0.6666666666
+print(2 % 3)  # 2
+print(2 ** 3) # 8
