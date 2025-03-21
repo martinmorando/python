a320f069ea305f7cb8a7a6187b412ed602e18b09
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-21 13:06:38 -0300

    add example for printing multiple elements and refactor comments

--- core/001-basics.py
+++ core/001-basics.py
@@ -8,11 +8,18 @@
     Multi-line comment
 '''
 
-# Declare and initiate variables
+# [DECLARE AND INITIATE VARIABLES]
+# - String
 name = "Clark Kent"
+# - Integer
 n = 1
+# - Boolean
 b = True
 
 
-# Output to the console
-print(name)
+# [OUTPUT]
+# - Output a single element to the console
+print(name)         # "Clark Kent"
+
+# - Output multiple elements to the console
+print(name, n)      # "Clark Kent" 1
