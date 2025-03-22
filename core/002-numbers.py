c043c80a10613bb872f4a56448f6f17178e19907
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-22 16:05:26 -0300

    add integer, float and augmented assignment operators in 002-numbers.py

--- core/002-numbers.py
+++ core/002-numbers.py
@@ -2,13 +2,28 @@
     Numbers
 '''
 
-# Declare and assign numeric value
-x = 3.14
+# [DECLARE AND ASSIGN A NUMERIC VALUE]
+# - Integer
+count = 1
 
-# Basic operations
-print(2 + 3)  # 5
-print(2 - 3)  # -1
-print(2 * 3)  # 6
-print(2 / 3)  # 0.6666666666
-print(2 % 3)  # 2
-print(2 ** 3) # 8
+# - Float
+price = 3.99    
+
+
+
+# [BASIC MATH OPERATIONS]
+print(2 + 3)        # Ouput: 5
+print(2 - 3)        # Ouput: -1
+print(2 * 3)        # Ouput: 6
+print(2 / 3)        # Ouput: 0.6666666666666666
+print(2 % 3)        # Ouput: 2
+print(2 ** 3)       # Ouput: 8
+
+
+
+# [AUGMENTED ASSIGNMENT OPERATORS]
+# - Plus-equals operator
+count += 2          # count = 3
+
+# - Minus-equals operator
+count -= 1          # count = 2
