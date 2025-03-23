f7f1d35101323e72cdf3e698b7496b97d24f436f
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-23 19:16:22 -0300

    add 003-errors.py and rename subsequent files to maintain order

---
+++ 003-errors.py
@@ -0,0 +1,18 @@
+'''
+    Errors
+
+    A) SyntaxError: invalid syntax.
+
+    B) NameError: trying to access a variable or function that has not been defined or 
+       is out of the scope.
+
+    C) TypeError: an operation is used on the wrong type of object.
+'''
+
+# print("Bitcoin      Output: SyntaxError: unterminated string literal (detected at line 12)
+
+# print(a)            Output: NameError: name 'a' is not defined
+
+# prin(a)             Output: NameError: name 'prin' is not defined. Did you mean: 'print'?
+
+# print('a' + 1)      Output: TypeError: can only concatenate str (not "int") to str
