034453463e544571c041a404ad9b114b4640a8ed
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-23 19:12:37 -0300

    add example of multi-line string in 008-strings.py

--- 008-strings.py
+++ 008-strings.py
@@ -2,6 +2,19 @@
     Strings
 '''
 
+# Multi-line strings (""" or ''')
+multiLine = """
+A purely peer-to-peer version of electronic cash would allow online
+payments to be sent directly from one party to another without going through a
+financial institution. Digital signatures provide part of the solution, but the main
+benefits are lost if a trusted third party is still required to prevent double-spending.
+We propose a solution to the double-spending problem using a peer-to-peer network.
+The network timestamps transactions by hashing them into an ongoing chain of
+hash-based proof-of-work, forming a record that cannot be changed without redoing
+the proof-of-work.
+"""
+
+
 # Concatenation
 a = "Hello"
 b = "Goodbye"
