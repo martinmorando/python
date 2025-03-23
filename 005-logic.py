26ad157e0f37432d9409f6d3c34445a03f73629a
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-23 19:11:34 -0300

    add boolean operators in 005-logic.py

--- 005-logic.py
+++ 005-logic.py
@@ -5,6 +5,12 @@
 a = 2
 b = 3
 
+# Boolean operators: and, or, not
+print((a == 2) and (b == 3))        # Output: True
+print((a == 2) or (b == 4))         # Output: True
+print(not a == 1)                   # Output: True
+
+
 # Conditional
 if (a > b):
     print("a > b")
@@ -12,3 +18,15 @@ elif (a < b):
     print("a < b")
 else:
     print("a = b")
+
+
+# Match statements, only valid since Python 3.10
+match a:
+    case 0:
+        print("Case", 0)
+    case 1:
+        print("Case", 1)
+    case 2:
+        print("Case", 2)
+    case default:
+        print("?")
