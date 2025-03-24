ebe391fcd8c769f2d66d31c2fcd9236f253835a7
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-24 08:46:29 -0300

    add .insert(), .count(), sorted(), more examples on slicing and sorting lists, and fix comment on len() in 005-lists.py

--- 005-lists.py
+++ 005-lists.py
@@ -2,15 +2,14 @@
     Lists
 '''
 
-# [Create lists]
+# [CREATE LISTS]
 empty = []
 strings = ["A", "B", "C"]
 numbers = [1, 2, 3]
 mixed = ["A", 1, 0.99, True]
+repetitive_list = ["C", "A", "C", "B", "A"]
 
-
-
-# - Create a list with integers from 0 up to 99 included
+# - Create a consecutive list with integers from 0 up to 99 included
 # (list() is necessary because range() returns a range object)
 list(range(100))            # [0, 1, 2, 3,..., 98, 99]
 
@@ -19,65 +18,95 @@ list(range(20, 100, 5))     # [20, 25, 30, 35,..., 90, 95]
 
 
 
-# [Count items inside list]
+# [LENGTH]: get the number of elements inside a list
 len(mixed)                  # 4
 
 
+# [COUNT]: count specific element inside a list
+repetitive_list.count("C")  # 2
 
-# [Access]: the 1st item of "strings" list
+
+
+# [ACCESS ITEM]: the 1st item of "strings" list
 strings[0]
 
 
 
-# [Add]: an item to "strings" list
-strings.append("D")         # ["A", "B", "C", "D"]
+# [MODIFY ITEM]: the 1st item from "strings" list
+strings[0] = "AA"           # 'AA'
 
 
 
-# [Remove]
+# [ADD ITEMS]
+# - Add an item to the end of "strings" list
+strings.append("D")         # strings: ['A', 'B', 'C', 'D']
+
+# - Insert by index (can use negative indexes)
+strings.insert(1, "A2")     # strings: ['A', 'A2', 'B', 'C', 'D']
+
+
+
+# [REMOVE ITEMS]
 # - Remove a specific item (first match only)
-strings.remove("B")         # ["A", "C", "D"]
+strings.remove("B")         # strings: ['A', 'A2' 'C', 'D']
 
 # - Remove the last item
-strings.pop()               # ["A", "C"]
+strings.pop()               # strings: ['A', 'A2', 'C']
 
 
 
-# [Modify]: the 1st item from "strings" list
-strings[0] = "AA"           # ["AA", "C"]
-
-
-
-# [Add lists]
+# [ADD LISTS]
 # - Add 2 lists
-mixed_2 = strings + numbers # ["AA", "C", 1, 2, 3]
+mixed_2 = strings + numbers # ['AA', 'A2', 'C', 1, 2, 3]
 
 # - Add a list that contains one item
-mixed_3 = strings + [4]     # ["AA", "C", 4]
+mixed_3 = strings + [4]     # ['AA', 'A2', 'C', 4]
 # This is wrong: mixed_4 = strings + 4
 
 
 
-# [Zip]: combine 2 lists into a list of tuples, where each tuple
-# contains one element from each list at the same index
-list(zip(numbers, strings))  # [(1, 'AA'), (2, 'C')]
-
-
-
-# [Slice]
-example_list = list(range(3, 11))   # [3, 4, 5..., 9, 10]
+# [SLICE]: extract a portion of a list
+example_list = list(range(3, 11))   # [3, 4, 5, 6, 7, 8, 9, 10]
 
 # - Slice the list to get elements from index 3 to 4 (index 5 is excluded)
 example_list[3:5]                   # [6, 7]
 
+# - Slice the list to get elements from index 0 to index 3 included
+example_list[:4]                    # [3, 4, 5, 6]
+
+# - Slice the list to get all elements except last 2
+example_list[:-2]                   # [3, 4, 5, 6, 7, 8]
+
+# - Slice the list to get all elements from index 5 to the end
+example_list[5:]                    # [8, 9, 10]
 
 
-# [Sort]
-# - Sort ascending order
+
+# [SORT]
 another_list = [3, 7, 1, 5, 8]
+disordered_letters = ["d", "a", "c", "g"]
+
+# - Sort ascending order
 another_list.sort()                 # None (no return value, sorts directly)
 print(another_list)                 # [1, 3, 5, 7, 8]
+
+disordered_letters.sort()
+print(disordered_letters)           # ['a', 'c', 'd', 'g']
+
 
 # - Sort descending order
 another_list.sort(reverse=True)     # None (no return value, sorts directly)
 print(another_list)                 # [8, 7, 5, 3, 1]
+
+
+
+# [SORTED]: similar to sort but creates a new list
+disordered_letters_2 = ["d", "a", "c", "g"]
+ordered_letters = sorted(disordered_letters_2)
+print(ordered_letters)              # ['a', 'c', 'd', 'g']
+
+
+
+# [ZIP]: combine 2 lists into a list of tuples, where each tuple
+# contains one element from each list at the same index
+list(zip(numbers, strings))         # [(1, 'AA'), (2, 'A2'), (3, 'C')]
