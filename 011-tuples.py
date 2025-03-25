09327f2e0fe1b6cf63d87af0748af42c33eeb894
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-25 19:54:26 -0300

    add min(), max() and a tuple of 1 item in 011-tuples.py

--- 011-tuples.py
+++ 011-tuples.py
@@ -1,10 +1,16 @@
 '''
     Tuples
-    - inmutable: can't be changed, can't add or remove elements
-    - think of fixed collections of items
+    - Built-in data structure; more efficient in memory and time compared to lists.
+    - Immutable: can't be changed, can't add or remove elements.
+    - Think of them as fixed collections of items.
+    - Can hold objects of different data types.
 '''
 
+# Define a tuple of 3 items
 t = ("apple", "banana", "orange")
+
+# Define a tuple of 1 item. Notice the comma
+t2 = ("apple",)
 
 # Access 2nd item
 firstItem = t[1]              # "banana"
@@ -17,3 +23,9 @@ index = t.index("orange")     # 2
 
 # Count number of occurences
 count = t.count("banana")     # 1
+
+# Min & Max: returns the maximum value. All items must be of the same type
+# - Numerical values: highest value
+# - String values: sorts alphabetically (closer to "Z", higher)
+print(min(t))   # apple
+print(max(t))   # orange
