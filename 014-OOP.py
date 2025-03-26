1b029ee0f0c03b5ae13d690424f53d7aa2fcde60
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-26 20:51:12 -0300

    add string representation, class variable, hasttr() and getattr() in 013-OOP.py

--- 014-OOP.py
+++ 014-OOP.py
@@ -6,15 +6,41 @@
 # PEP 8 Style: capitalize names of classes 
 class Calculator:
 
+    author = "Martin"   # This is a class variable
+
     # Constructor
     def __init__(self): # This is also a dunder method
         print("START")
+
+    # String representation
+    def __repr__(self):
+        return "I'm a calculator"
 
     # Method
     def addition(self, n1, n2):
         return n1 + n2
 
 
+# Initiate object
 calculate = Calculator()
+
+# Print attribute
+print(calculate.author)                      # Output: Martin
+
+# Call method
 result = calculate.addition(3, 4)  
-print(result)                # Output: 7
+print(result)                                # Output: 7
+
+# Print object
+print(calculate)                             # Output: I'm a calculator
+
+
+
+# [BUILT-IN FUNCTIONS]
+# - hasttr()
+print(hasattr(calculate, "author"))          # Output: True
+print(hasattr(calculate, "year"))            # Output: False
+
+# - getattr()
+print(getattr(calculate, "author"))          # Output: Martin
+print(getattr(calculate, "year", "default")) # Output: default
