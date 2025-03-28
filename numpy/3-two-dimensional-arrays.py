4922483d47ce6e69d9108b7dba307fdfbdf95ece
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-28 13:35:09 -0300

    add 3-two-dimensional-arrays.py

---
+++ numpy/3-two-dimensional-arrays.py
@@ -0,0 +1,26 @@
+'''
+    Two-dimensional arrays
+    - They have 2 axes:
+        - Axis 0: values that are in the same column ("same indexical position")
+        - Axis 1: values that are in the same row ("share an array")
+'''
+
+import numpy as np 
+
+# [DEFINE]
+# Define a 2 dimensional-array
+a = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+# This WON'T WORK. Notice the []
+# a = np.array([1, 2, 3, 4, 5], [6, 7, 8, 9, 10])
+
+# This also works
+b = np.array([[1, 2, 3, 4, 5],   # First row
+              [6, 7, 8, 9, 10]]) # Second row
+
+
+
+# [SELECT ELEMENT]
+# - [ROW, COLUMN]
+# - Example: 2nd row (index 1), 4th column (index 3)
+print(b[1, 3])                   # Output: 9
