8d1e3b1aee271e6873392933e50d2ece251c2865
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-29 11:13:46 -0300

    add matrix product, examples on how to select element/s, and polish comments in 001-1D-arrays.py

--- numpy/001-1D-arrays.py
+++ numpy/001-1D-arrays.py
@@ -2,28 +2,53 @@
     1D arrays
 '''
 
-# Import NumPy
+# [IMPORT LIBRARY]
 import numpy as np
 
-# Create an array from a list of integers
-a = [1, 2, 3, 4, 5, 6]
-arrA = np.array(a)
-print(arrA)
 
 
-a = np.array([1, 2, 3, 4, 5])
+# [CREATE 1D ARRAY]
+# - Create an array from a list of integers
+a_list = [1, 2, 3, 4, 5, 6]          # Python list
+a = np.array(a_list)                 # Numpy array: [1 2 3 4 5 6]
 
-addition = a + 3            # [4 5 6 7 8]
-subtraction = a - 3         # [-2 -1 0 1 2]
-multiplication = a * 3      # [3 6 9 12 15]
-division = a / 2            # [0.5 1. 1.5 2. 2.5]
-squared = a ** 2            # [1 4 9 16 25]
-squared_root = np.sqrt(a)   # [1. 1.41421356 1.73205081 2. 2.23606798]
+# - Create an array
+b = np.array([1, 2, 3, 4, 5])
 
 
-b = np.array([6, 7, 8, 9, 10])
 
-print(a + b)                # [7 9 11 13 15]
-print(a - b)                # [-5 -5 -5 -5 -5]
-print(a * b)                # [6 14 24 36 50]
-print(a / b)                # [0.16666667 0.28571429 0.375 0.44444444 0.5]
+# [OPERATIONS WITH SCALARS]
+addition = b + 3            # [4 5 6 7 8]
+subtraction = b - 3         # [-2 -1 0 1 2]
+multiplication = b * 3      # [3 6 9 12 15]
+division = b / 2            # [0.5 1. 1.5 2. 2.5]
+squared = b ** 2            # [1 4 9 16 25]
+squared_root = np.sqrt(b)   # [1. 1.41421356 1.73205081 2. 2.23606798]
+
+
+
+# [OPERATIONS BETWEEN ARRAYS]
+c = np.array([6, 7, 8, 9, 10])
+print(b + c)                # [7 9 11 13 15]
+print(b - c)                # [-5 -5 -5 -5 -5]
+# - Elementwise product
+print(b * c)                # [6 14 24 36 50] = [1*6 + 2*7 + 3*8 + 4*9 + 5*10]
+# - Matrix product (2 alternatives, same result)
+print(b @ c)                # 130
+print(b.dot(c))             # 130
+
+
+# [SELECT AN ITEM]: returns 1 item
+# - Select 4th item
+print(c[3])                 # 9
+# - Select last item
+print(c[-1])                # 10
+
+
+
+# [SELECT SEVERAL ITEMS]: returns an array
+# - Including a[1] but excluding a[3]
+print(c[1:3])               # [7 8]
+
+# - Last 2 elements
+print(c[-2:])               # [9 10]
