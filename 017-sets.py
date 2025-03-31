8428d1f26325ba7f6fc08d09f9689765477f8a7e
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-31 18:51:08 -0300

    add 017-sets.py

---
+++ 017-sets.py
@@ -0,0 +1,25 @@
+'''
+    Sets
+    - Group of unordered elements and no duplicates.
+    - Can contain any combination of data types.
+'''
+
+# [CREATE]
+# - Alternative A: using {}
+lettersA = {"A", "B", "C", "D", "D"}
+print(lettersA)              # Output: {'B', 'C', 'A', 'D'}
+
+# - Alternative B: using set()
+lettersB = set(["A", "B", "C", "D", "D"])
+print(lettersB)              # Output: {'A', 'C', 'B', 'D'}
+
+
+
+# [ADD]
+# - Add 1 element to a set
+lettersA.add("E")
+print(lettersA)              # Output: {'D', 'B', 'A', 'E', 'C'} 
+
+# - Add several elements to a set
+lettersA.update(["F", 1.23])
+print(lettersA)              # Output: {1.23, 'B', 'C', 'E', 'A', 'D', 'F'}
